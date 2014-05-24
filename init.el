9c1a18fac638365c3e7a7380976a14a6f3549e31
John Jorgensen <john@2gn.com>
2014-05-24 08:44:57 -0400

    Added some comments

--- init.el
+++ init.el
@@ -16,6 +16,8 @@
 (setq ido-create-new-buffer 'always)
 
 ;; always kill the buffer on exit
+;; seems to produce the same result as
+;; (setq kill-buffer-query-functions nil)
 (setq kill-buffer-query-functions
   (remq 'process-kill-buffer-query-function
     kill-buffer-query-functions))
@@ -27,7 +29,6 @@
 (setq apropos-do-all t)
 
 ;; Backup file buffer settings.
-;; (setq make-backup-files nil)
 (setq
  backup-by-copying t                ; don't clobber symlinks
  backup-directory-alist
