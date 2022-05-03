30ce45965967fd479cf38e48048e49f6b3d865ab
John Jorgensen <jjorgensen@hsdp.io>
2022-05-03 11:54:01 -0400

    move bindings out of the way of ansi-term

--- init.el
+++ init.el
@@ -11,7 +11,6 @@
                     :height 150
                     )
 
-;;; Code:
 ;; no gnu splash screen on startup
 (setq inhibit-splash-screen t)
 
@@ -201,14 +200,15 @@
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 (setq ivy-count-format "(%d/%d) ")
+(setq ivy-wrap t)
 
 (global-set-key (kbd "C-s") 'swiper-isearch)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "M-y") 'counsel-yank-pop)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> f") 'counsel-describe-function)
+(global-set-key (kbd "<f2> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f2> l") 'counsel-find-library)
 (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
 (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
 (global-set-key (kbd "<f2> j") 'counsel-set-variable)
